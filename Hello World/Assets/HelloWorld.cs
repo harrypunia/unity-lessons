8c2cd56be8e16aebcdbded861fd608d31c73349e
Harry <harryrock67@gmail.com>
2019-12-15 15:44:07 -0500

    Hello World

---
+++ Hello World/Assets/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+    
+    void Start() {// Start is called before the first frame update
+        print("Hello world");
+    }
+    
+    void Update() { // Update is called once per frame
+        
+    }
+}
